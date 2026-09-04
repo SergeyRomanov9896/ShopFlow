57e3d8be6bb12e50bfef8add3c93cba14ecfb2b8
SergeyRomanov9896 <automag2016@mail.ru>
2026-09-04 14:46:05 +0300

    Enhance Product and Category classes with string representation and addition method

--- src/shopflow/shop.py
+++ src/shopflow/shop.py
@@ -28,6 +28,9 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self) -> str:
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
     @property
     def price(self) -> int | float:
         return self.__price
@@ -47,6 +50,12 @@ class Product:
         quantity = product_data["quantity"]
 
         return cls(name, description, price, quantity)
+
+    def __add__(self, other) -> int | float:
+        """Возвращает сумму полных стоимостей двух товаров на складе (цена * количество)."""
+        self_total = self.price * self.quantity
+        other_total = other.price * other.quantity
+        return self_total + other_total
 
 
 class Category:
@@ -77,6 +86,9 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def __str__(self) -> str:
+        return f"{self.description}, количество продуктов: {self.product_count} шт."
+
     @property
     def products(self) -> str:
         result = []
@@ -98,35 +110,20 @@ if __name__ == "__main__":
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3],
     )
 
+    print(str(category1))
+
     print(category1.products)
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
 
-    new_product = Product.new_product(
-        {
-            "name": "Samsung Galaxy S23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5,
-        }
-    )
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
